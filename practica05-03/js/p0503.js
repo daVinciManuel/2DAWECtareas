b8e7ec842ddc6a16b9f074cad850726445a25c25
daVinciManuel <mgmt.manuel02@gmail.com>
2023-09-25 21:35:46 +0200

    FINISHED practica05-03

--- practica05-03/js/p0503.js
+++ practica05-03/js/p0503.js
@@ -1,9 +1,9 @@
-console.log('Hello world :3')
+console.log('hello')
 window.onload = comienzo;
 
-function comienzo(){
+function comienzo(){ 
     document.formulario.boton.onclick = tratar;
-}
+ }
 function tratar(){
 // RECIBO LOS VALORES DEL TEXTAREA Y LOS CONVIERTO A INT
     let VstNumA = document.formulario.inicio.value;
@@ -17,9 +17,27 @@ function tratar(){
         VitNumA = VitNumB;
         VitNumB = aux;
     }
-    console.log('jejej tengo el boton')
     console.log("first number: ",VitNumA);
     console.log("second number: ",VitNumB);
 
+    let VstSolucion = "|";
+
+    for(let i = VitNumA; i <= VitNumB; i++){
+        if(numeroPrimo(i)){
+            VstSolucion += i.toString() + "|";
+        }
+    }
+    console.log(VstSolucion)
+    document.formulario.resultado.value = VstSolucion 
 }
 
+function numeroPrimo(numero) {
+    let VboPrimo = true;
+    for (let i = 2; i < numero; i++){
+        if(numero % i == 0)
+            VboPrimo = false
+    }
+    console.log(VboPrimo, numero);
+    
+    return VboPrimo;
+}
