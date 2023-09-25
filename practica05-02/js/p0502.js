8c0e453c26be5b5548e4a9aa7f5e3dd0c5967506
daVinciManuel <mgmt.manuel02@gmail.com>
2023-09-25 13:31:58 +0200

    ADDED: P0502 RESUELTO

--- practica05-02/js/p0502.js
+++ practica05-02/js/p0502.js
@@ -1,4 +1,4 @@
-console.log('Hello World :v')
+console.log('hello world')
 
 window.onload = comienzo;
 
@@ -8,22 +8,27 @@ window.onload = comienzo;
 
 // window.onclick = comienzo;
 function comienzo(){
-    let VstResuelto="|";
-    for(let i = 2; i < 100; i++){
+    let VstResuelto="";
+    let i = 2;
+    let contador = 0;
+    while(contador<100){
         if(numeroPrimo(i)){
             VstResuelto += i.toString() + "|";
+            contador++;
         }
+        i++;
+        console.log(contador)
     }
     document.formulario.box.value = VstResuelto;
 }
 
 function numeroPrimo(numero) {
     let VboPrimo = true;
-    for (let i = 2; i < numero; ++i){
+    for (let i = 2; i < numero; i++){
         if(numero % i == 0)
             VboPrimo = false
     }
-    // console.log(VboPrimo, numero);
+    console.log(VboPrimo, numero);
     
     return VboPrimo;
 }
