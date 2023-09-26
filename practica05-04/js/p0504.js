398f382b3f7d811d5a04f53e355f2c4d5fb0c4ea
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-09-26 11:44:24 +0000

    Added practica05-04

---
+++ practica05-04/js/p0504.js
@@ -0,0 +1,55 @@
+console.log('Hello world');
+
+window.onload = function (){
+    document.formulario.btn.onclick = tratar;
+}
+function tratar(){
+    
+    const VstNumber = document.formulario.decimal.value;
+    const VitNumber = parseInt(VstNumber);
+    // console.log(VitNumber)
+    // console.log('Hello I\'m from tratar()');
+    
+    let nBinario = binaryOf(VitNumber)
+    let nOctal = octalOf(VitNumber)
+    let nHex = hexOf(VitNumber)
+    document.formulario.binario.vale = nBinario;
+    document.formulario.octal.value = nOctal;
+    document.formulario.hexadecimal.value = nHex;
+}
+
+// ------------- CONVIERTE A BINARIO ----------------------
+function binaryOf(number){
+    if(number == 0){
+        solucion = 0;
+    } else{
+        solucion = "";
+        while(number > 0.999){
+            resto = (number % 2);
+            solucion = resto.toString() + solucion.toString();
+            number = Math.trunc(number / 2);
+        }
+    }
+    return solucion;
+}
+
+// ------------- CONVIERTE A OCTAL ----------------------
+function octalOf(number){
+    if(number == 0){
+        solucion = 0;
+    } else{
+        solucion = "";
+        while(number > 0.999){
+            resto = (number % 8);
+            solucion = resto.toString() + solucion.toString();
+            number = Math.trunc(number / 8);
+        }
+    }
+    return solucion;
+}
+
+// ------------- CONVIERTE A HEXADECIMAL ----------------------
+function hexOf(number){
+    solucion = number.toString(16);
+    return solucion;
+}
