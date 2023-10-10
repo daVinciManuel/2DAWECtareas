e89b62499363ffff91a48c2b06b45ee51029e374
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-10-10 12:20:09 +0000

    update: p0507.js

--- practica05-07/js/p0507.js
+++ practica05-07/js/p0507.js
@@ -3,19 +3,37 @@ console.log('Hello world')
 window.onload = () => {
     document.formu.btn.onclick = verificar
 }
+const nombre = document.formu.nombre.value
 function verificar() {
-    const nombre = document.formu.nombre.value
-    console.log(document.formu.nombre.value)
-    document.formu.msg.value = rmWhiteSpace(nombre);
-    console.log(rmWhiteSpace(nombre))
+    // console.log(document.formu.nombre.value)
+    // document.formu.msg.value = nombre;
+
+    letraInicioYFinal(document.formu.nombre.value)
 }
-// function rmWhiteSpace(t){
-//     newString = "";
-//     newString = t.replace(" ","")
-//     if(newString != t){
-//         newString = rmWhiteSpace(newString);
-//         return newString
-//     }else{
-//         return newString
-//     }
-// }
+function letraInicioYFinal(t) {
+    let firstChar = t.charAt(0)
+    let lastChar = t.charAt(t.length - 1)
+    debug = true
+    if(debug){
+        console.log()
+        console.log("first: " + firstChar + " es una letra " + esLetra(firstChar))
+        console.log("last: " + lastChar + " es una letra " + esLetra(lastChar))
+        console.log("letraInicioYFinal: " + t)
+    }
+    // QUEDA HACER EL MENSAJE DE ERROR !!!
+    if(!esLetra(firstChar) || !esLetra(lastChar)){
+
+        let error = esLetra(firstChar) ? lastChar : firstChar 
+        document.formu.msg.value = error;
+        // document.formu.msg.value = "El caracter " + !esLetra(firstChar) ? firstChar : 'último' + " no es una letra"
+    }
+}
+// this fn returns true or false wether the x is a letter or not 
+function esLetra(x) {
+    let letras = "çqwertyuiiopasdfghjklñzxcvbnmáéíóúü"
+    if (letras.includes(x.toLowerCase())) {
+        return true
+    } else {
+        return false
+    }
+}
