172092e5a7444d28c227b7e38779f488b7896c96
daVinciManuel <mgmt.manuel02@gmail.com>
2023-10-11 09:45:07 +0200

    update: p0507.js

--- practica05-07/js/p0507.js
+++ practica05-07/js/p0507.js
@@ -9,31 +9,43 @@ function verificar() {
     // document.formu.msg.value = nombre;
 
     letraInicioYFinal(document.formu.nombre.value)
+    lengthValidate(document.formu.nombre.value)
 }
 function letraInicioYFinal(t) {
     let firstChar = t.charAt(0)
     let lastChar = t.charAt(t.length - 1)
-    debug = true
+    debug = false; 
     if(debug){
         console.log()
         console.log("first: " + firstChar + " es una letra " + esLetra(firstChar))
         console.log("last: " + lastChar + " es una letra " + esLetra(lastChar))
         console.log("letraInicioYFinal: " + t)
     }
-    // QUEDA HACER EL MENSAJE DE ERROR !!!
-    if(!esLetra(firstChar) || !esLetra(lastChar)){
 
-        let error = esLetra(firstChar) ? lastChar : firstChar 
-        document.formu.msg.value = error;
-        // document.formu.msg.value = "El caracter " + !esLetra(firstChar) ? firstChar : 'último' + " no es una letra"
+    // MENSAJE DE ERROR:
+    if(!esLetra(firstChar) || !esLetra(lastChar)){
+        if(!document.formu.msg.value.includes("1.")){
+            document.formu.msg.value = document.formu.msg.value.concat("\n1. Debe empezar y terminar por un caracter del abecedario.");
+        }
+    }else{
+        document.formu.msg.value = document.formu.msg.value.replaceAll("1. Debe empezar y terminar por un caracter del abecedario.","")
     }
 }
 // this fn returns true or false wether the x is a letter or not 
 function esLetra(x) {
-    let letras = "çqwertyuiiopasdfghjklñzxcvbnmáéíóúü"
+    let letras = "qwertyuiiopasdfghjklñzxcvbnmáéíóúüç"
     if (letras.includes(x.toLowerCase())) {
         return true
     } else {
         return false
+    }
+}
+// LONGITUD MINIMA 3 CHAR
+function lengthValidate(t){
+    if(t.length < 3){
+        if(!document.formu.msg.value.includes("2."))
+            document.formu.msg.value = document.formu.msg.value.concat("\n 2. Debe contener más de 3 caracteres.")
+    }else{
+        document.formu.msg.value = document.formu.msg.value.replaceAll("2. Debe contener más de 3 caracteres.","")
     }
 }
