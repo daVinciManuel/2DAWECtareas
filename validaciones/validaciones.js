3c2227d15a51f7e53dd7e40bd1c11864246942ce
daVinciManuel <145429825+daVinciManuel@users.noreply.github.com>
2023-11-10 13:13:58 +0000

    update validaciones.js

--- validaciones/validaciones.js
+++ validaciones/validaciones.js
@@ -10,12 +10,16 @@ function esNif(s){
         if(s.length != 9){
             return 0;
         }else{
-            if(formatoNIE(s) || formatoNIF(s)){
-                return false;
-                
+            if(!formatoNIF(s)){
+                return 0
+            }else{
+
             }
         }
 }
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// falta modularizar codigo para distinguir casos de error
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 function formatoNIF(s){
         if(isNaN(s.slice(0,8)) && !("trwagmyfpdxbnjzsqvhlcke".includes(s.charAt(8)))){
             return false;
@@ -24,10 +28,29 @@ function formatoNIF(s){
 function formatoNIE(s){
     let firstChar = s.charAt(0);
         if(!("XYZLKM".includes(firstChar)) && isNaN(s.slice(1,8)) && !("trwagmyfpdxbnjzsqvhlcke".includes(s.charAt(8)))){
-            return false;
+            return 0;
         }else{
+            // variable para el caso en el que hay que sumar algo por la letra inicial
+            valor = 0;
             switch (firstChar){
-                "X"
+                case "Y": 
+                    valor = 1;
+                    break;
+                case "X":
+                    valor = 2;
+                    break;
+                default:
+                    valor = 0;
+                break;
+            }
+            num = parseInt(s.substring(1,s.length-1))
+            resto = num % 23;
+            finalChars = ["T","R","W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J","Z","S","Q","V","H","L","C","K","E"];
+            correctFinalChar = finalChars[resto];
+            if(s.slice(-1) != correctFinalChar){
+                return 2; // es incorrecto
+            }else if(num > 100000){
+                return 3; // es DNI
             }
         }
 }
